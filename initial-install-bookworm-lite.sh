b8543adc8d74662556509fc9655a19ff8b7b73fb
Brendan Arthur Ring <brendanarthurring@gmail.com>
2024-10-28 11:55:19 +0900

    Update initial-install-bookworm-lite.sh
    added changing gpio to lgpio

--- initial-install-bookworm-lite.sh
+++ initial-install-bookworm-lite.sh
@@ -7,8 +7,10 @@ sudo apt install -y \
 fbi \ # Software for showing images on the commandline
 neofetch \ # useful for checking linux distro info
 
-# to install on os
-sudo apt install -y python3-picamera2 
+# to install on raspberry pi os lite with no gui support
+sudo apt install -y python3-picamera2 --no-install-recommends
 
-# for using gui
-sudo apt install -y python3-pyqt5 python3-opengl
+# to prevent RuntimeError: Error waiting for edge when using GPIO.wait_for_edge(4, GPIO.FALLING)
+sudo apt remove python3-rpi.gpio
+sudo apt install python3-rpi-lgpio
+sudo apt autoremove
