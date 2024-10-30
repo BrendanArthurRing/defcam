da0082a372f5936b82abfda9e1449ad50bb1d826
Brendan Arthur Ring <brendanarthurring@gmail.com>
2024-10-30 10:24:13 +0900

    Update initial-install-bookworm-lite.sh
    added pisugar requirements

--- initial-install-bookworm-lite.sh
+++ initial-install-bookworm-lite.sh
@@ -6,6 +6,7 @@ sudo apt update
 sudo apt install -y \ 
 fbi \ # Software for showing images on the commandline
 neofetch \ # useful for checking linux distro info
+i2c-tools # needed for using pisugar
 
 # to install on raspberry pi os lite with no gui support
 sudo apt install -y python3-picamera2 --no-install-recommends
@@ -14,3 +15,18 @@ sudo apt install -y python3-picamera2 --no-install-recommends
 sudo apt remove python3-rpi.gpio
 sudo apt install python3-rpi-lgpio
 sudo apt autoremove
+
+# Get current state of i2c, should be off (set to 1) by default at install
+#sudo raspi-config nonint get_i2c
+
+# Enable i2c
+sudo raspi-config nonint do_i2c 0
+
+# detect i2c bus and devices
+i2cdetect -y 1
+i2cdump -y 1 0x32
+i2cdump -y 1 0x75
+
+wget https://cdn.pisugar.com/release/pisugar-power-manager.sh
+bash pisugar-power-manager.sh -c release
+
